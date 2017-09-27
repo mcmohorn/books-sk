abe6de22b1839dcd38e0155c08cbb0001ebb9706
Matt Mohorn <mcmohorn@gmail.com>
2017-09-27 16:21:09 -0400

    Mark $ as a global to ignore error

--- app/routes/books.js
+++ app/routes/books.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    return $.getJSON('https://skookum-test-api.herokuapp.com/api/v1/books')
+    return $.getJSON('https://skookum-test-api.herokuapp.com/api/v1/books'); /* globals $ */
   },
 });
